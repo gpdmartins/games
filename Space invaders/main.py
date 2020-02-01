aeb7756b178d430f24e3f11cb14132421f46d943
gpdmartins <gpdmartins@gmail.com>
2020-01-31 23:15:20 -0300

    Icon and display

---
+++ Space invaders/main.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+#initialize the pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+#Title and Icon
+pygame.display.set_caption("Space_Invaders")
+icon = pygame.image.load("space-ship.png")
+pygame.display.set_icon(icon)
+
+#Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((0,0,0))
