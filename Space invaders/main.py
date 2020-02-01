31750c65e896d74d1b75cd62f3e3211404bfe458
gpdmartins <gpdmartins@gmail.com>
2020-01-31 23:41:13 -0300

    comments, player img and positioning

--- Space invaders/main.py
+++ Space invaders/main.py
@@ -7,15 +7,30 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
-#Title and Icon
+# Caption and Icon
 pygame.display.set_caption("Space_Invaders")
 icon = pygame.image.load("space-ship.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+# put the img at the initial position
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 #Game Loop
 running = True
 while running:
+
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill((0,0,0))
+
+
+    player()
+    pygame.display.update()
