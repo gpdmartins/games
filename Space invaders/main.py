fab2fb9dd3c45e75299fcb5149870d95be23d20a
gpdmartins <gpdmartins@gmail.com>
2020-02-01 11:57:53 -0300

    Enemy movement, when it hits the boarde, it will go down.

--- Space invaders/main.py
+++ Space invaders/main.py
@@ -21,7 +21,8 @@ playerX_change = 0
 enemyImg = pygame.image.load('enemy.png')
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
-enemyX_change = 0
+enemyX_change = 0.3
+enemyY_change = 40
 
 
 def player(x, y):
@@ -56,12 +57,24 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
+    # Checking for boundaries of spaceships so it doesn't get out of bounds
+    playerX += playerX_change
+
     if playerX <= 0:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
 
-    playerX += playerX_change
+    # Enemy movement
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
     pygame.display.update()
